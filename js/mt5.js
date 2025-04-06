3ee0afe054e348a4b970344e045f76f14b5e9bc5
LeiiZun <104386657+LeiiZun@users.noreply.github.com>
2025-04-07 00:31:28 +0200

    Delete js directory

--- js/mt5.js
+++
@@ -1,5 +0,0 @@
-// Exemple de code pour gérer la connexion à MetaTrader 5
-function connectMT5() {
-    // Exemple de code pour la connexion à MetaTrader 5
-    alert("Connexion à MetaTrader 5 en préparation...");
-}
